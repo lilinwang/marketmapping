132bc21d1d65933607ccd237c52ed9cea392d30a
lilian <lilianwang@sjtu.edu.cn>
2014-12-01 00:52:10 -0500

    Front-end: revised all front-end framework

--- application/views/home.php
+++ application/views/home.php
@@ -1,24 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="utf-8">
 	
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	
 	<title >MarketMapping</title>
+	
 	<link rel="stylesheet" href="http://apps.bdimg.com/libs/bootstrap/3.2.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/css.css">
-	<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
-		
+	<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">		
+    <!-- MetisMenu CSS -->
+    <link href="css/plugins/metisMenu/metisMenu.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/sb-admin-2.css" rel="stylesheet">	
+	
 	<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
 	<script src="http://code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
 	<script type="text/javascript" src="js/jquery.boutique_min.js"></script>
 	<script type="text/javascript" src="https://www.google.com/jsapi"></script>	
 	<script type="text/javascript" src="js/html2canvas.js" type="text/javascript"></script>
 	
-\
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="js/plugins/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="js/sb-admin-2.js"></script>    
+    <script type="text/javascript" src="js/bootstrap-filestyle.js"> </script>
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	
 <script type="text/javascript">	
 	var company_id=-1;		
 	var urlimg="";
 	var axisnum=0;
+	var logo_show=0;
 	
 	$(document).ready(function() {
 		var $logos=$("#logos"),
@@ -32,8 +64,8 @@
 				
 				var dropElem = ui.draggable;				
 				dropElem.css('position', 'absolute');
-				dropElem.css('top', ui.position.top+$(this).offset().top-125);
-				dropElem.css('left', ui.position.left+$(this).offset().left-50);
+				dropElem.css('top', ui.position.top+$(this).offset().top+111);
+				dropElem.css('left', ui.position.left+$(this).offset().left-370);
 
 				$(this).append(dropElem);
 				//$(this).append(ui.draggable.css({position: 'static'});
@@ -56,6 +88,15 @@
       		        }
       	        });
       	});
+		$('body').on('click','#view_logo',function(){
+			if (logo_show){
+				$("#sidebar").hide();
+				logo_show=0;
+			}else{
+				$("#sidebar").show();
+				logo_show=1;
+			}			
+		});
 		//$("a",$logos).draggable();
 		/*$("#draggable1").draggable({			
 			stop: function( event, ui ) {
@@ -101,230 +142,154 @@
 		});				            			     	
 	};			
 	
-	function get_metrics(){
-			//document.getElementById("focus").className="btn btn-default";
-			//ok.className ="btn btn-success";			
-			$.post("ajax/get_metrics", 
-			{
-				
-            },			
-            function(data,status){
-				console.log(data);
-				data = eval("(" + data + ")");
-				var innerHTML3="<p>Choose x metrics:</p>";	
-				console.log(data);		
-				var ndata=new Array('Complexity','Price','Quality','Customer Segment','Security','Research Style','Risk','Value','Reputation','Customization');
-				for(var i = 0; i < ndata.length; i++)
-	            {
-					innerHTML3 += "<input class=\"btn btn-default\" name=\"metric1\" onclick=\"drawChart(this,"+i+",1)\" type=\"button\" value=\""+ndata[i]+"\" />";
-				}
-				document.getElementById("metrics1").innerHTML = innerHTML3;	
-				innerHTML2="<p>Choose y metrics:</p>";
-				for(var i = 0; i < ndata.length; i++)
-	            {
-					innerHTML2 += "<input class=\"btn btn-default\" name=\"metric0\" onclick=\"drawChart(this,"+i+",0)\" type=\"button\" value=\""+ndata[i]+"\" />";
-				}
-				/*for(var i = 0; i < data.metrics.length; i++)
-	            {
-	            	innerHTML2 += "<input class=\"btn\" onclick=\"drawChart(this,value,"+i+",1)\" type=\"button\" value=\""+data.metrics[i].metric+"\" />";					
-				}	
-				for(var i = 0; i < data.metrics.length; i++)
-	            {
-					innerHTML2 += "<input class=\"btn\" onclick=\"drawChart(this,value,"+i+",0)\" type=\"button\" value=\""+ndata[i]+"\" />";
-	            }*/
-				document.getElementById("metrics2").innerHTML = innerHTML2;				
-			});
-	};	
-     google.load("visualization", "1", {packages:["corechart"]});
-    // google.setOnLoadCallback(drawChart);
-	var columnx=0;
-	var columny=1;
-	var valuex='Complexity';
-	var valuey='Price';
-     function drawChart(value,column,isx) {						
-		//alert(value+valuex+valuey);		
-		if (isx==1){
-			columnx=column;valuex=value.value;	
-			var m1=document.getElementsByName("metric1");
-			for (var i=0;i<m1.length;i++){
-				m1[i].className ="btn btn-default";				
-			};			
-		}else {
-			columny=column;valuey=value.value;
-			var m0=document.getElementsByName("metric0");
-			for (var i=0;i<m0.length;i++){
-				m0[i].className ="btn btn-default";
-			};	
-		}
-		value.className ="btn btn-success";	
-		
-		//=document.getElementById("columnx").value;
-		//=document.getElementById("columny").value;
-		//alert(value+valuex+valuey);
-        var data = new google.visualization.DataTable();		
-		data.addColumn('number','Complexity');
-		data.addColumn('number','Price');
-		data.addColumn('number','Quality');
-		data.addColumn('number','Customer Segment');
-		data.addColumn('number','Security');
-		data.addColumn('number','Research Style');
-		data.addColumn('number','Rist');
-		data.addColumn('number','Value');
-		data.addColumn('number','Reputation');
-		data.addColumn('number','Customization');
-		data.addColumn('string','Company');
-		data.addColumn({type: 'string', role: 'tooltip', p: {'html': true}});
-		
-		data.addRows([
-			[7,1,3,2,1,4,5,4,10,9,'Intercom.io','<img width=100px src="image/intercom.png">'],
-			[3,4,9,9,1,4,4,8,4,3,'Mixpanel.com','<img width=100px src="image/mixpanel.png">'],
-			[3,5,4,4,1,4,4,7,5,2,'getvero.com','<img width=100px src="image/getvero.png">'],
-			[1,1,7,4,3,4,5,5,5,6,'segment.io','<img width=100px src="image/segment.png">'],
-			[9,9,9,6,8,7,7,6,5,8,'mailchimp.com','<img width=100px src="image/mailchimp.png">']        
-        ]);
-		var options = {
-		  tooltip: {isHtml: true},
-          title: valuex+' vs. '+ valuey +' comparison',
-          hAxis: {title: valuex, gridlines:{count:2},minValue: 0,maxvalue: 10, baseline:5 },
-          vAxis: {title: valuey, gridlines:{count:2},baseline:5, minValue: 0, maxValue:10 },
-          legend: 'none',
-		  pointSize: 15,
-		  series: {
-                0: { pointShape: 'circle' },
-                1: { pointShape: 'triangle' },
-                2: { pointShape: 'square' },
-                3: { pointShape: 'diamond' },
-                4: { pointShape: 'star' }
-            },
-		  animation: {
-			duration: 5000,
-			easing: 'In',
-		  }		 
-        };			  
-  // Create and draw the visualization.
-		var chart = new google.visualization.ScatterChart(document.getElementById('chart_div'));
-		function selectHandler() {
-          var selectedItem = chart.getSelection()[0];
-          if (selectedItem) {
-            //var topping = data.getValue(selectedItem.row, 1);
-			window.open('http://'+data.getValue(selectedItem.row, 10));
-            //alert('The user selected ' + topping);
-          }
-        }	
-		var view = new google.visualization.DataView(data);
-		view.setColumns([columnx,columny,11]);
-		google.visualization.events.addListener(chart, 'select', selectHandler);          				      						
-		chart.draw(view, options);
-		
-    } 		  
-</script>
-		
-	<script type="text/javascript">		
-
-	function exportimg(fileName) {
-    // for non-IE
-	var fileURL='http://localhost/marketmapping/servlet?img=' + urlimg;
-    if (!window.ActiveXObject) {
-        var save = document.createElement('a');
-        save.href = fileURL;
-        save.target = '_blank';
-        save.download = fileName || 'unknown';
-
-        var event = document.createEvent('Event');
-        event.initEvent('click', true, true);
-        save.dispatchEvent(event);
-        (window.URL || window.webkitURL).revokeObjectURL(save.href);
-    }
-
-    // for IE
-    else if ( !! window.ActiveXObject && document.execCommand)     {
-        var _window = window.open(fileURL, '_blank');
-        _window.document.close();
-        _window.document.execCommand('SaveAs', true, fileName || fileURL)
-        _window.close();
-    }
-	}
-
-    function exportimg(value){
-		var img = '/flower.png';
-		//file_put_contents(img, file_get_contents(urlimg));
-		jQuery.ajax({
-
-			url: 'http://localhost:8080/marketmapping/servlet?img=' + urlimg,
-			//data: myData,
-			type: 'GET',
-			crossDomain: true,
-			dataType: 'jsonp',
-		// success: function() { alert("Success"); },
-		// error: function() { alert('Failed!'); },
-		// beforeSend: setHeader
-		});
-	}
-	</script>
+</script>		
 </head>
+
 <body>
 
-<div id="container">
-	<img width=200px src="image/Logo-V1-Gaps.png"/> <h1></h1>
-	
-	<div id="body">
-	<div id="search">
-		<p>Search company name:</p>
-		<div class="col-lg-6">
-		<div class="input-group">
-			<input type="text" class="form-control" id="company_name" />
-			<span class="input-group-btn">
-				<button class="btn btn-default" onclick="search_name()" name="submit" type="button">Search</button>
-			</span>
-		</div>
-		<!-- /input-group <input type="text" class="input"  /> <input type="submit" class="btn"  value="search"/>chart_div	-->		
-		
-		</div>	
-		<div class="col-lg-6">
-		<div class="input-group">
-		
-			<input type="file" class="form-control" id="userfile" name='userfile' />
-			<span class="input-group-btn">
-				<button class="btn btn-default" onclick="upload()" name="submit" type="button">Upload</button>
-			</span>
-		</div>		
-		</div>
-		<!--<button class="btn btn-default" onclick="get_metrics()" name="submit" type="button">Show axis</button>-->		
-		<button class="btn btn-default" id="save_image" name="submit" type="button">Export Image</button>
-	</div>
-	
-	<div class="logos-class">
-		<div id="logos">		
-		</div>
-	</div>
-	</br></br>
-	<div id="metrics" >	
-		<div id="metrics1" class="btn-group">
-		</div>
-		<div id="metrics2" class="btn-group">
-		</div>
-	</div>
+    <div id="wrapper">
 
-	
-	<div class="myImage">
-		          			         		      	  				  
-	<div id="squares">   
-		<div id="upper" style="border-bottom:1px solid;height:450px;width:898px;">   
-			<div id="square1" style="border-right: 1px solid;height:450px;width:450px;"></div>	
-		</div>		
-		<div id="lower" style="height:450px;width:900px;">   
-			<div id="square4" style="border-right: 1px solid;height:448px;width:450px;"></div>
-		</div>
-	</div>	
-	</div>
-     
-				
-	<div id="custom_html_content_div"></div>
-		<div id="chartContainer" style="width: 900px; height: 500px;"></div>	
-	</div>
-	
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>	
-	
-</div>
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#" id="view_logo">
+					<i class="fa fa-align-justify"></i>
+					Logos
+				</a>				
+            </div>
+            <!-- /.navbar-header -->
+
+            <ul class="nav navbar-top-links navbar-right">
+                <!-- /.dropdown<button class="btn btn-default" id="save_image" name="submit" type="button">Export Image</button>
+                     -->				
+                <li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#" >
+                        Save <i class="fa fa-cloud-download"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-tasks">
+                        <li id="canvas-style" style="margin-left:10px">							                             
+                            <p>
+                                <strong>Style:</strong>
+                            </p>							
+							<button class="btn" id="canvas-white"><img  src="img/style138.png" /></button>
+							<button class="btn" id="canvas-gray"><img  src="img/style139.png" /></button>
+							<button class="btn" id="canvas-ivory"><img  src="img/style140.png" /> </button>
+													
+                        </li>
+                        <li class="divider"></li>
+						<li>
+                            <a href="#" id="save_image" >
+								Save as PNG  
+                            </a>
+                        </li>                        
+                                                
+                    </ul>
+                    <!-- /.dropdown-tasks -->
+                </li>
+                
+            </ul>
+            <!-- /.navbar-top-links -->
+
+            <div class="navbar-default sidebar" role="navigation" id="sidebar">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li class="sidebar-search">								
+                            <div class="input-group custom-search-form">
+                                <input type="text" class="form-control" placeholder="Search..." id="company_name">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" onclick="search_name()" name="submit" type="button">
+                                        <i class="fa fa-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                            <!-- /input-group -->
+                        </li>
+                        <li>
+							<div  class="input-group" style="width:260px;margin-left:15px;height:50px;">
+							<span class="input-group-btn">
+								<span class="input-group-btn">
+								<input type="file" class="filestyle" id="userfile" name='userfile' >
+								</span>
+                                <span class="input-group-btn">
+									
+                                    <button class="btn btn-default" onclick="upload()" name="submit" type="button" >
+                                        <i class="fa fa-upload"></i>
+                                    </button>
+                                </span>
+							</span>
+                            </div>						
+                        </li>                        
+						<li>
+							<div class="logos-class">
+								<div id="logos">		
+								</div>
+							</div>
+						</li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <!-- Page Content -->
+        <div id="page-wrapper">
+            <div class="container-fluid">
+				<div class="myImage">
+					<div class="map-title">
+						<p contenteditable="true" style="text-align:center">xx vs. competitors</p>     
+					</div>
+					
+					<div id="squares">   
+						<div id="upper" style="border-bottom:1px solid;height:450px;width:898px;">   
+							<div id="square1" style="border-right: 1px solid;height:450px;width:450px;"></div>	
+						</div>		
+						<div id="lower" style="height:450px;width:900px;">   
+							<div id="square4" style="border-right: 1px solid;height:448px;width:450px;"></div>
+						</div>
+					</div>	
+					<div class="label-top">
+						<p contenteditable="true" style="text-align:center">High price</p>     			         		      	  				  
+					</div>
+					<div class="label-bottom">
+						<p contenteditable="true" style="text-align:center">Low price</p>     			         		      	  				  
+					</div>
+					<div class="label-left">
+						<p contenteditable="true" style="text-align:center">Difficult to use</p>     			         		      	  				  
+					</div>
+					<div class="label-right">
+						<p contenteditable="true" style="text-align:center">Easy to use</p>     			         		      	  				  
+					</div>
+				</div>               
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    
+<script type="text/javascript">
+	$('#canvas-white').click(function(e) {
+		e.stopPropagation();
+		$('#squares').css('background','#FFFFFF');
+	});
+	$('#canvas-gray').click(function(e) {
+		e.stopPropagation();
+		$('#squares').css('background','#C0C0C0');
+	});
+	$('#canvas-ivory').click(function(e) {
+		e.stopPropagation();
+		$('#squares').css('background','#FFFFF0');
+	});
+</script>
 </body>
+
 </html>
